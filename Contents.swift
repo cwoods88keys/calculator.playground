a8019489c02ee8467e87a8605c5e6e1c12f69f84
cwoods88keys <prc-lh138-14@PRC-LH138-14s-iMac.local>
2016-07-11 15:45:34 -0500

    calculator
    playing with operators

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let num1 : Double = 5
+let num2 : Double = 2
+let num3 = 0.5
+
+let sum = num1 + num2
+let product = num1 * num2
+let quotient = num1 / num2
+let difference = num1 - num2
+let pct = num3 * 100
+
+print("Sum: ", sum)
+print("Difference: \(difference)")
+print("Product: \(product)")
+print ("Quotient: ", quotient)
+print ("\(num3) is \(pct)%")
+
+
+
+
+
+
+
